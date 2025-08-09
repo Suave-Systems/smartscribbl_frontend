4797c0370c619fe4179fd66c0c65ee14bf5ab140
oluseunDesina <desinaoluseun@gmail.com>
2025-08-09 08:46:56 +0100

    Changed the backend base URL

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  baseUrl: 'https://smartscribbl-backend.onrender.com/',
+  baseUrl: 'https://smartscribbl-dev-10ee31f735b9.herokuapp.com/',
+  // baseUrl: 'https://smartscribbl-backend.onrender.com/',
 };
